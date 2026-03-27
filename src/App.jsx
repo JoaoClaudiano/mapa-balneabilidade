50097370d5476d1c3695562292b8edc58e528b01
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-03-27 00:36:51 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Map from './components/Map';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Balneabilidade do Litoral Cearense</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
